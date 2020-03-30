9b6d026e0b62a9e77332b4ee08aaa802c087ac71
mac <tchikh3@gmail.com>
2020-03-30 23:53:30 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoronaComponent } from './corona/corona.component';
 
 
 const routes: Routes = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NvbarComponent } from './nvbar/nvbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
+import { CoronaComponent } from './corona/corona.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NvbarComponent,
+    FooterComponent,
+    HeadComponent,
+    BodyComponent,
+    CoronaComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
